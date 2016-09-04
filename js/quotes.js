fae2263ef870b089698d26ab2048b71e227b6d54
damofer <damofer2004@hotmail.com>
2016-09-03 21:15:57 -0500

    After Feedback modifications
    * for in  ' hasOwnProperty  filter added-
    *Semicolons on lines 51, 52 and 53 added.
    *Re-structured function getQuote and renamed to getRandomQuote.

--- js/quotes.js
+++ js/quotes.js
@@ -1,6 +1,6 @@
 var quotes = [
 	{
-		quote: "The best preparation for good work tomorrow is to do good work today.",
+		quote: " 1 The best preparation for good work tomorrow is to do good work today.",
 		source : "Elbert Hubbard",
 		citation : "BrainyQuote.com",
 		year: 2016,
@@ -8,35 +8,35 @@ var quotes = [
 	},
 
 	{
-		quote: "Individual commitment to a group effort - that is what makes a team work, a company work, a society work, a civilization work.",
+		quote: " 2 Individual commitment to a group effort - that is what makes a team work, a company work, a society work, a civilization work.",
 		source : "Vince Lombardi",
 		citation : "BrainyQuote.com",
 		year: 2016,
 		tags: ['Work','Politics']
 	},
 	{
-		quote: "Talk is cheap. Show me the code.",
+		quote: " 3 Talk is cheap. Show me the code.",
 		source : "Linus Torvalds",
 		citation : "Techsource",
 		year: 2000,
 		tags: ['IT','Programming']
 	},
 	{
-		quote: "I invented the term 'Object-Oriented', and I can tell you I did not have C++ in mind.",
+		quote: " 4 I invented the term 'Object-Oriented', and I can tell you I did not have C++ in mind.",
 		source : "Alan Kay",
 		citation : "Techsource",
 		year: 2005,
 		tags: ['IT','Programming']
 	},
 	{
-		quote: "They don't make bugs like Bunny anymore.",
+		quote: " 5 They don't make bugs like Bunny anymore.",
 		source : "Olav Mjelde",
 		tags: ['IT','Programming']
 
 
 	},
 	{
-		quote: "Perfection [in design] is achieved, not when there is nothing more to add, but when there is nothing left to take away.",
+		quote: " 6 Perfection [in design] is achieved, not when there is nothing more to add, but when there is nothing left to take away.",
 		source : " Antoine de Saint-Exupéry",
 		tags: ['Design','Programming', 'IT']
 
--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ document.getElementById('loadQuote').addEventListener("click", printQuote, false
 
 var timeoutID;
 var quoteList;
-initQuoteList();
+getRandomQuote(quoteList);
 printQuote();
 
 
@@ -16,7 +16,7 @@ quoteList = shuffleArray(quoteList);
 }
 
 
-// This function shuffles an array  randomly
+// Knuth shuffle algorithm , This function shuffles an array  randomly
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -31,7 +31,11 @@ function shuffleArray(array) {
 	Then it renew the array once it has no more items on it- EXTRA CREDITS
 
 */
-function getQuote(quoteList){
+function getRandomQuote(quoteList){
+	if(quoteList === undefined){
+		initQuoteList();
+		return 0;
+	}
 	if(quoteList.length === 1){
 		initQuoteList();
 	}
@@ -39,22 +43,14 @@ function getQuote(quoteList){
 
 }
 
-//This function is not used for the extra credits. but it just get a random quote from an array.
-/*
-function getRandomQuote(quotes){
-	var x = Math.floor(Math.random()*quotes.length)
-	var quote = quotes[x];
-// return 1 random quote from the quotes array.
-	return quote;
-};
-*/
+
 
 // function that  sets body background-color randomly--EXTRA CREDITS
 function randomRGB(){
 
-	var r = Math.floor(Math.random()*256)
-	var g = Math.floor(Math.random()*256)
-	var b = Math.floor(Math.random()*256)
+	var r = Math.floor(Math.random()*256);
+	var g = Math.floor(Math.random()*256);
+	var b = Math.floor(Math.random()*256);
 	var rgbColor = 'rgb(' + r + ',' + g + ',' + b + ')';
 	document.body.style.backgroundColor = rgbColor;
 }
@@ -64,23 +60,27 @@ function printQuote(){
 // clear timeOut once it enter this function
  window.clearTimeout(timeoutID);
 // get a quote object from  quotes array.
-	var quote = getQuote(quoteList);
+	var quote = getRandomQuote(quoteList);
 	var citation='';
 	var year ='';
 	var tags ='';
 
 // search for optional properties inside object. EXTRA CREDITS
 	for( var key in quote){
-		if(key === 'citation'){
-			citation ='<span class="citation">'+ quote[key]+ '</span>';
+		//filter the prototype property.
+		if (quote.hasOwnProperty(key)) {
 
-		}
-		if(key === 'year'){
-			year ='<span class="year">'+ quote[key]+ '</span>';
-		}
-		if(key === 'tags'){
-			tags ='<span class="tags">['+ quote[key].join(', ')+ ']</span>';
-		}
+			if(key === 'citation'){
+				citation ='<span class="citation">'+ quote[key]+ '</span>';
+
+			}
+			if(key === 'year'){
+				year ='<span class="year">'+ quote[key]+ '</span>';
+			}
+			if(key === 'tags'){
+				tags ='<span class="tags">['+ quote[key].join(', ')+ ']</span>';
+			}
+    	}
 	}
 // displays quote's property values in the html.
 	var quoteBox = document.getElementById('quote-box');
